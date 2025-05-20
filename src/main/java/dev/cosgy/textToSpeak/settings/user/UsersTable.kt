84efb61a5632e74022bb9c8662e60b3fa5cbb983
yuyaprgrm <yuyaprgrm@outlook.jp>
2025-05-21 06:53:41 +0900

    remove unnecessary type hint

--- src/main/java/dev/cosgy/textToSpeak/settings/user/UsersTable.kt
+++ src/main/java/dev/cosgy/textToSpeak/settings/user/UsersTable.kt
@@ -5,11 +5,10 @@ import org.jetbrains.exposed.v1.core.dao.id.EntityID
 import org.jetbrains.exposed.v1.core.dao.id.IdTable
 
 object UsersTable : IdTable<Long>("users") {
-    override val id: Column<EntityID<Long>> = long("id").entityId()
-    val voiceModel: Column<String> = varchar("voice_model", 255) // common fs limitation?
-
-    val speed: Column<Float> = float("speed")
-    val intonation: Column<Float> = float("intonation")
-    val voiceQualityA: Column<Float> = float("voice_quality_a")
-    val voiceQualityFm: Column<Float> = float("voice_quality_fm")
+    override val id = long("id").entityId()
+    val voiceModel = varchar("voice_model", 255) // common fs limitation?
+    val speed = float("speed")
+    val intonation = float("intonation")
+    val voiceQualityA = float("voice_quality_a")
+    val voiceQualityFm = float("voice_quality_fm")
 }
